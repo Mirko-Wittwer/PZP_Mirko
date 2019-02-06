5e5e1a4301790b2f2b30565301b21f90a7930b1e
Mirko <starforce64@gmx.de>
2019-02-06 08:58:36 +0100

    Gitignore

---
+++ Kneipe/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String kleiner16 = "Eine Auswahl von Sprudel, Cola, Fanta, Apfelschorle";
+        String bis17 = "Eine Auswahl von Sprudel, Cola, Fanta, Apfelschorle, Trollinger, Riesling, Stuttgarter Hofbräu";
+        String ueber18 = "Eine Auswahl von Sprudel, Cola, Fanta, Apfelschorle, Trollinger, Riesling, Stuttgarter Hofbräu, Jägermeister, Absinth";
+
+        int gast = 55;
+
+        if (gast < 16) {
+            System.out.print(kleiner16);
+        } else if (gast < 18) {
+            System.out.print(bis17);
+        } else {
+            System.out.print(ueber18);
+        }
+    }
+}
---
+++ WieloeseicheinProblem/src/Main.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // https://www.java-forum.org/thema/vergleichen-von-strings.1350/
+
+        /*System.out.println("Gib einen Wert ein!");
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        System.out.println("Die Zahl " + i + " wurde eingeben.");
+        System.out.println("Gib einen Wert ein!");
+
+        Scanner sc = new Scanner(System.in);
+        String i = sc.next();
+
+        System.out.println("Die Wert " + i + " wurde eingeben.");*/
+
+
+        System.out.println("Funktioniert alles wie es soll? Ja oder Nein?");
+        Scanner sc = new Scanner(System.in);
+
+        String funktion = sc.next();
+
+        if(funktion.equals("ja")){
+
+            System.out.println("Las es so! Es gibt keine Probleme!");
+
+        }else{
+            System.out.println("Hast Du dran rumgefummelt? Ja oder Nein?");
+        }
+
+        Scanner sc2 = new Scanner(System.in);
+        String rumgefummelt = sc2.next();
+
+        if(rumgefummelt.equals("ja")){
+            System.out.println("Du Blödmann!");
+            System.out.println("Weiß jemand davon? Ja oder nein?");
+
+            Scanner sc3 = new Scanner(System.in);
+            String bescheid = sc3.next();
+            if(bescheid.equals("ja")){
+                System.out.println("Du armes Schwein!");
+            }else{
+                System.out.println("Behalts für Dich!");
+            }
+
+        }else{
+            System.out.println("Bist Du dafür verantwortlich? Ja oder Nein?");
+
+            Scanner sc4 = new Scanner(System.in);
+            String verantwortlich = sc4.next();
+
+            if(verantwortlich.equals("ja")){
+                System.out.println("Kannst Du es jemand anderem anhängen? Ja oder Nein?");
+
+                Scanner sc5 = new Scanner(System.in);
+                String anhaengen = sc5.next();
+
+                if(anhaengen.equals("nein")){
+                    System.out.println("Du armes Schwein!");
+                }else{
+                    System.out.println("Es gibt kein Problem!");
+                }
+
+
+            }else{
+                System.out.println("Mehr Glück als Verstand gehabt! Es gibt kein Problem!");
+            }
+        }
+
+
+    }
+}
