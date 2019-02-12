90c6fd175a7f6a141ee804c992bf3aed63329eb3
Mirko <starforce64@gmx.de>
2019-02-12 17:26:34 +0100

    Tag7 Nachmittag

--- Telefonauswahlmenue/src/Main.java
+++ Telefonauswahlmenue/src/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Telefonmenü
+        // 1 - Info über Produkte
+            // 1 - Neues Microsoft Surface
+            // 2 - Neues Office Suite
+            // 3 - Zurück zum Hauptmenü
+        // 2 - Produktregistrierung
+            // 1 - Frage nach der Seriennummer
+            // 2 - Ausgabe: Seriennummer wurde aktiviert
+        // 3 - Verbindung zum Kundensupport
+        // 4 - Beenden
+
 
         Scanner sc = new Scanner(System.in);
 
@@ -11,13 +22,42 @@ public class Main {
 
         boolean nochmal = true;
 
-        while(nochmal){
-            System.out.println("1: Infos über unsere Produkte");
-            System.out.println("2: Produktregistrierung");
-            System.out.println("3: Kundensupport");
-            System.out.println("4: Ende");
+
+
+        while (nochmal){
+            System.out.println("Wählen Sie zwischen: \n 1. Info über Produkte. \n 2. Produktregistrierung. \n 3. Verbindung zum Kundensupport. \n 4. Beenden.");
 
             int auswahl = sc.nextInt();
+
+            switch (auswahl){
+
+                case 1:
+                    System.out.println("Info über Produkte");
+                    System.out.println("1. Neues Microsoft Surface");
+                    System.out.println("2. Neues Office Suite");
+                    System.out.println("3. Zurück zum Hauptmenü");
+                    int auswahl2 = sc.nextInt();
+                    break;
+                case 2:
+                    System.out.println("Produktregistrierung");
+                    System.out.println("1. Frage nach der Seriennummer");
+                    System.out.println("2. Ausgabe: Seriennummer wurde aktiviert");
+                    System.out.println("3. Zurück zum Hauptmenü");
+                    int auswahl3 = sc.nextInt();
+                    break;
+                case 3:
+                    System.out.println("Verbindung zum Kundensupport");
+                    int auswahl4 = sc.nextInt();
+                    break;
+                case 4:
+                    System.out.println("Benden");
+                    int auswahl5 = sc.nextInt();
+                    break;
+
+            }
+
         }
+
+
         }
     }
