70cbfe9b6a75c48ebedef93bd5f4521aee77b610
Mirko <starforce64@gmx.de>
2019-02-12 13:04:05 +0100

    Tag7 Vormittag

---
+++ Telefonauswahlmenue/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        boolean nochmal = true;
+
+        while(nochmal){
+            System.out.println("1: Infos über unsere Produkte");
+            System.out.println("2: Produktregistrierung");
+            System.out.println("3: Kundensupport");
+            System.out.println("4: Ende");
+
+            int auswahl = sc.nextInt();
+        }
+        }
+    }
